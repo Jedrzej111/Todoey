f85caa30a41c5996992d4265ba353f887f7cc8ba
Jędrzej Przykaza <jedrzejprzykaza@jedrzejnomacbook-pro.local>
2018-04-28 14:32:01 +0900

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jędrzej Przykaza on 28.04.2018.
+//  Copyright © 2018 Jedrzej Przykaza. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
